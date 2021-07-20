aa87b003ee311fb329978a4d6cfda02de4568194
Daniel Alves <daniel.alves@skytef.intranet>
2021-07-19 22:10:21 -0300

    Testes

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testIgnorePatterns: ["/node_modules", "/.next/"],
+    testPathIgnorePatterns: ["/node_modules", "/.next/"],
     setupTests: [
         "<rootDir>/src/tests/setupTests.ts"
     ],
