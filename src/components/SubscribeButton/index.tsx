fd1abef5bc2cefb18edb297b032fde7d0a8de87d
Daniel Alves <daniel.alves@skytef.intranet>
2021-07-21 21:10:32 -0300

    Testando SubscribeButton

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,42 +1,47 @@
-import { useSession, signIn } from 'next-auth/client';
-import { api } from '../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss';
+import { Session } from "next-auth"
+import { useSession, signIn } from "next-auth/client"
+import { useRouter } from "next/dist/client/router"
+import { api } from "../../services/api"
+import { getStripeJs } from "../../services/stripe-js"
+import styles from "./styles.module.scss"
 
-interface SubscribeButtonProps {
-    priceId: string;
+
+interface UserSubscriptionSession extends Session {
+    activeSubscription?: any;
 }
 
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    const [session] = useSession();
-    
+export type SessionProps = [UserSubscriptionSession, boolean]
+
+export default function SubscribeButton() {
+    const [session]: SessionProps = useSession()
+    const router = useRouter()
+
     async function handleSubscribe() {
         if (!session) {
-            signIn('github')
-            return;
+            return signIn("github")
         }
 
-        try { 
-            const response = await api.post('/subscribe')
+        if (session?.activeSubscription) {
+            return router.push("/posts")
+        }
+
+        try {
+            const response = await api.post("/subscribe")
 
             const { sessionId } = response.data
 
             const stripe = await getStripeJs()
 
             await stripe.redirectToCheckout({ sessionId })
-             
-        } catch (err) {
-            alert(err.message)
+        } catch (error) {
+            alert(error.message)
         }
     }
-    
+
     return (
-        <button 
-            type="button"
+        <button
             className={styles.subscribeButton}
             onClick={handleSubscribe}
-        >
-            Subscribe now
-        </button>
+        >Subscribe now</button>
     )
 }
